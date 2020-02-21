00097ae756a4e8c5782436c1b3844988c68cd305
julie_tilatti <julie.tilatti@etu.u-bordeaux.fr>
2020-02-21 10:29:46 +0100

    niveau suivant relie

--- mysite/static/javascript/script.js
+++ mysite/static/javascript/script.js
@@ -1,52 +1,73 @@
-function check(event){
-    var x = event.clientX;        // x de la souris
-    var y = event.clientY;        // y de la souris
+function check(event) {
+    var x = event.clientX; // x de la souris
+    var y = event.clientY; // y de la souris
 
-    var height = document.getElementById('painting').offsetHeight;    //hauteur du tableau
-    var width = document.getElementById('painting').offsetWidth;      //largeur du tableau
+    var height = document.getElementById('painting').offsetHeight; //hauteur du tableau
+    var width = document.getElementById('painting').offsetWidth; //largeur du tableau
 
     var viewportOffset = painting.getBoundingClientRect();
-    var top = viewportOffset.top;             // y du point haut-gauche du cadre
-    var left = viewportOffset.left;           // x du point haut-gauche du cadre
+    var top = viewportOffset.top; // y du point haut-gauche du cadre
+    var left = viewportOffset.left; // x du point haut-gauche du cadre
 
-    scaleX =  width / 100;
+    var nb_level = 5;
+
+    scaleX = width / 100;
     scaleY = height / 100;
 
     var num = -1;
-    for (var i = 0; i < 5; i++) {
-      if((x >= left + (tab[i].x * scaleX))
-      && (x <= left + (tab[i].x * scaleX) + (tab[i].width * scaleX))
-      && (y >= top + (tab[i].y * scaleY))
-      && (y <= top + (tab[i].y * scaleY) + (tab[i].height * scaleY))){
-        num = i;
-        break;
-      }
+    for (var i = 0; i < nb_level; i++) {
+        if ((x >= left + (tab[i].x * scaleX)) &&
+            (x <= left + (tab[i].x * scaleX) + (tab[i].width * scaleX)) &&
+            (y >= top + (tab[i].y * scaleY)) &&
+            (y <= top + (tab[i].y * scaleY) + (tab[i].height * scaleY))) {
+            num = i;
+            break;
+        }
+    }
+    if (num > -1 && num < nb_level) {
+        tab[num].checked = true;
     }
 
-    tab[num].checked = true;
     /////////TO DO : AJAX --> mettre true dans la BDD /////////////////////////////
 
-    $('.details div:nth-child('+ (num + 1) +') img').addClass("check");
+    $('.details div:nth-child(' + (num + 1) + ') img').addClass("check");
+
+    var end = true;
+    for (var i = 0; i < nb_level; i++) {
+        if (tab[i].checked == false) {
+            end = false;
+        }
+
+    }
+    if (end) {
+        document.getElementById('nextlevel').style.display = 'block';
+    }
 }
+
 
 
 
 ////////    TODO : une seule fonction popup    //////////
-function openPopUpClue(){
+function openPopUpClue() {
     document.getElementById('popup').style.display = 'block';
 }
-function closePopUpClue(){
+
+function closePopUpClue() {
     document.getElementById('popup').style.display = 'none';
 }
-function OpenClueText(){
+
+function OpenClueText() {
     document.getElementById('clueText').style.display = 'block';
 }
-function CloseClueText(){
+
+function CloseClueText() {
     document.getElementById('clueText').style.display = 'none';
 }
-function OpenNoCluePopUp(){
+
+function OpenNoCluePopUp() {
     document.getElementById('noClueAvalaible').style.display = 'block';
 }
-function CloseNoCluePopUp(){
+
+function CloseNoCluePopUp() {
     document.getElementById('noClueAvalaible').style.display = 'none';
 }
