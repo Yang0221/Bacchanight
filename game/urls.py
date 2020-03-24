64cc083adba11eecf41c1b54f3e23dec11304d78
jtilatti <julie.tilatti@etu.u-bordeaux.fr>
2020-03-24 18:19:16 +0100

    commentaires

--- game/urls.py
+++ game/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('continue' , views.form_user_continue , name = 'form_user_continue'),
     path('add_user', views.add_user, name = "add_user"),
     path('recover_user', views.recover_user, name = "recover_user"),
-    path('next_level', views.next_level, name = "next_level")
-
-
-
+    path('next_level', views.next_level, name = "next_level"),
+    
+    #lié au AJAX
+    #path('check_detail', views.check_detail, name = "check_detail")
 ]
